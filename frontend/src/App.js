ad6a201a39e3203eac4d5c331478be846125e520
Tihomir Puskaric <tihomir.625@gmail.com>
2022-03-28 16:10:34 +0200

    exercise form, chakra icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import TrainingForm from "./pages/TrainingForm";
 
+import { Box } from "@chakra-ui/react";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={"Log In"} />
-        <Route exact path="/create" element={<TrainingForm />} />
-      </Routes>
-    </Router>
+    <Box m="50px">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={"Log In"} />
+          <Route exact path="/create" element={<TrainingForm />} />
+        </Routes>
+      </Router>
+    </Box>
   );
 }
 
--- frontend/src/pages/TrainingForm.js
+++ frontend/src/pages/TrainingForm.js
@@ -1,19 +1,87 @@
 import React from "react";
 
-import { Box } from "@chakra-ui/react";
+import { Box, FormLabel, Input, FormControl, IconButton, Grid, Button } from "@chakra-ui/react";
+import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
+
+import axios from "axios";
 
 const TrainingForm = () => {
-  const [data, setData] = React.useState(null);
+  const [formData, setFormData] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch("http://localhost:4001/api")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => setData(data.message));
-  }, [data]);
+  const handleAddExercise = () => {
+    setFormData([...formData, { name: "", sets: "", reps: "" }]);
+  };
 
-  return <Box>{!data ? "Loading..." : data}</Box>;
+  const handleDeleteExercise = (i) => {
+    setFormData(formData.filter((_, ind) => ind !== i));
+  };
+
+  const handleChange = (event, i) => {
+    let newFormData = [...formData];
+    newFormData[i][event.target.name] = event.target.value;
+
+    setFormData(newFormData);
+  };
+
+  const handleSubmit = () => {
+    axios
+      .post("http://localhost:4001/api/create", formData)
+      .then(() => console.log("Form data sent"))
+      .catch((err) => {
+        console.error(err);
+      });
+
+    setFormData([]);
+  };
+
+  return (
+    <FormControl>
+      <IconButton mb={4} icon={<AddIcon />} colorScheme="blue" aria-label="Dodaj vježbu" onClick={handleAddExercise} />
+      <Grid templateColumns="repeat(4, 1fr)">
+        {formData.map((el, i) => {
+          return (
+            <Box key={i} mb={12} maxW="320px">
+              <Box mb={4}>
+                <FormLabel htmlFor={`name${i}`}>Ime vježbe</FormLabel>
+                <Input
+                  id={`name${i}`}
+                  name="name"
+                  type="text"
+                  value={el.name}
+                  onChange={(event) => handleChange(event, i)}
+                />
+              </Box>
+              <Box mb={4}>
+                <FormLabel htmlFor={`sets${i}`}>Broj setova</FormLabel>
+                <Input
+                  id={`sets${i}`}
+                  name="sets"
+                  type="number"
+                  value={el.sets}
+                  onChange={(event) => handleChange(event, i)}
+                />
+              </Box>
+              <Box mb={4}>
+                <FormLabel htmlFor={`reps${i}`}>Broj ponavljanja u setu</FormLabel>
+                <Input
+                  id={`reps${i}`}
+                  type="number"
+                  value={el.reps}
+                  name="reps"
+                  onChange={(event) => handleChange(event, i)}
+                />
+              </Box>
+
+              <IconButton icon={<DeleteIcon />} colorScheme="red" onClick={() => handleDeleteExercise(i)} />
+            </Box>
+          );
+        })}
+      </Grid>
+      <Button isDisabled={formData.length ? false : true} colorScheme="blue" onClick={handleSubmit}>
+        Spremi
+      </Button>
+    </FormControl>
+  );
 };
 
 export default TrainingForm;
