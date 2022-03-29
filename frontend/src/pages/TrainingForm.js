bb30ac1b49495b5c04150ba3bde4651f4fef725c
Tihomir Puskaric <tihomir.625@gmail.com>
2022-03-29 12:24:15 +0200

    rm import

--- frontend/src/pages/TrainingForm.js
+++ frontend/src/pages/TrainingForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
 
 import { Box, FormLabel, Input, FormControl, IconButton, Grid, Button } from "@chakra-ui/react";
 import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
